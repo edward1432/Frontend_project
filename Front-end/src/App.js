4d4e0cde3030434e647884bc9466e35c9c276b0a
Edward Todd <edwardtdd@gmail.com>
2022-06-20 14:03:15 +0100

    a

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -2,9 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Museums from './components/Museums/Museums';
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './pages/Home';
+import MuseumLandingPage from './components/MuseumLandingPage/MuseumLandingPage';
+import ArtefactsLandingPage from './components/ArtefactsLandingPage/ArtefactsLandingPage';
+import ExhibitLandingPage from './components/ExhibitLandingPage/ExhibitLandingPage';
+import StaffLandingPage from './components/StaffLandingPage/StaffLandingPage';
 
 function App() {
 
@@ -13,7 +17,11 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" exact component={<Home />} />
+          <Route path='/' element={<Home />}/>
+          <Route path='/museumslanding' element={<MuseumLandingPage />}/>
+          <Route path='/artefactslanding' element={<ArtefactsLandingPage />}/>
+          <Route path='/exhibitlanding' element={<ExhibitLandingPage />}/>
+          <Route path='/stafflanding' element={<StaffLandingPage />}/>
         </Routes>
       </Router>
     </div>
