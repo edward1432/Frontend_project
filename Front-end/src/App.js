0d36804b37c0e0f957e5c2a97dab1c57626f0485
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2022-06-20 12:22:38 +0100

    created home, title + search bar

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -2,13 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Museums from './components/Museums/Museums';
 import { useState } from "react";
+import Header from './components/Header/Header';
+import HomePage from "./components/HomePage/HomePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className='App'>
+      <Router>
+        <Header />
+          <Routes>
+            <Route path="/home" exact component={HomePage} />
+          </Routes>
+      </Router>
+    </div>
   )
 }
 
--- Front-end/src/components/Header/Header.js
+++ Front-end/src/components/Header/Header.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './Header.css';
+import { Link } from "react-router-dom";
 
-const Header = () => (
-  <div className="Header">
-    Header Component
-  </div>
-);
+function Header () {
 
-Header.propTypes = {};
+  const [openLinks, setOpenLinks] = useState(false);
 
-Header.defaultProps = {};
+  const toggleNavBar = () => {
+    setOpenLinks(!openLinks);
+  };
+
+  return (
+    <div className='navBar'>
+
+      <div className='leftSide' id={openLinks ? "open" : "close"}>
+        <div className='Home'>
+          <Link to="/home">Home</Link>
+        </div>
+      </div>
+
+      <div className='middle'>
+        <div className='Title'>
+          <h1>Muse360</h1>
+        </div>
+      </div>
+
+      <div className='rightSide'>
+        <div className='SearchBar'>
+          <input type="text" placeholder="Search..."></input>
+        </div>
+
+        {/* <div className='DarkModeButton'>
+          const [theme, setTheme] = useState("dark");
+
+          const toggle Theme
+        </div> */}
+      </div>
+
+    </div>
+  )
+}
 
 export default Header;
