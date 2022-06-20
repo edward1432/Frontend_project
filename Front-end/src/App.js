57a77d05e359471793e757d39d33ef29defb2009
Edward Todd <edwardtdd@gmail.com>
2022-06-20 10:50:12 +0100

    A

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -2,13 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Museums from './components/Museums/Museums';
 import { useState } from "react";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from './components/Header/Header';
+import Home from './pages/Home';
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact component={<Home />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
---
+++ Front-end/src/pages/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return(
+        <>This is the homepage</>
+    )
+}
+
+export default Home;
