86968c68254024ed7cd84c148b95b0cdf8d02b77
Edward Todd <edwardtdd@gmail.com>
2022-06-20 09:03:31 +0100

    b

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,12 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Museums from './components/Museums/Museums';
+import { useState } from "react";
+
 
 function App() {
+
+  const [page, setPage] = useState("")
+
   return (
     <>
+      {/* <div>
+        { "" !== page && <button onClick={() => setPage('')}>Home</button>}
+       {page}<br/> </div>
+      {'museums' === page &&
+      <Museums />
+      }
+      { "" === page && <div>
+
+      <button onClick={() => setPage('museums')}>Museums</button><br></br>
+      <button onClick={() => setPage('exhibitions')}>Exhibitions</button><br></br>
+      <button onClick={() => setPage('artefacts')}>Artefacts</button><br></br>
+      <button onClick={() => setPage('staff')}>Staff</button><br></br>
+      </div> */}
+}
     
+
     </>
-  );
+  )
 }
 
 export default App;
