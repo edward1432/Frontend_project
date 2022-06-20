ad777d2e903835ebc2d6b24b301baeeedc4a2f45
Edward Todd <edwardtdd@gmail.com>
2022-06-20 09:48:19 +0100

    a

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -6,26 +6,8 @@ import { useState } from "react";
 
 function App() {
 
-  const [page, setPage] = useState("")
-
   return (
     <>
-      {/* <div>
-        { "" !== page && <button onClick={() => setPage('')}>Home</button>}
-       {page}<br/> </div>
-      {'museums' === page &&
-      <Museums />
-      }
-      { "" === page && <div>
-
-      <button onClick={() => setPage('museums')}>Museums</button><br></br>
-      <button onClick={() => setPage('exhibitions')}>Exhibitions</button><br></br>
-      <button onClick={() => setPage('artefacts')}>Artefacts</button><br></br>
-      <button onClick={() => setPage('staff')}>Staff</button><br></br>
-      </div> */}
-}
-    
-
     </>
   )
 }
