526b78ebf1627f4817f3c43fbc35ab7426177301
Edward Todd <edwardtdd@gmail.com>
2022-06-20 15:43:40 +0100

    Began landing page

--- Front-end/src/components/Museums/Museums.js
+++ Front-end/src/components/Museums/Museums.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Museums.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
-const Museums = () => (
-  <div className="Museums">
-    Museums Component
-  </div>
-);
+const Museums = () => {
+  const [museums, setMuseums] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8080/museum')
+    .then(res => {
+      const museums = res.data;
+      setMuseums(museums);
+    }).catch((err) => console.log(err));
+  }, []
+  );
+  return(
+    <ul>
+      {
+        museums.map(museum =>
+          <li key={museum.id}>{`${museum.name}`}</li>
+          )
+      }
+    </ul>
+  )
+
+}
 
 Museums.propTypes = {};
 
