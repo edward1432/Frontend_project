600a3da26f53c82c85321d8c0ec130f243e239a7
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2022-06-20 16:42:39 +0100

    current dark mode point

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,27 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Museums from './components/Museums/Museums';
-import { useState } from "react";
+import { createContext, useState } from "react";
 import Header from './components/Header/Header';
 import HomePage from "./components/HomePage/HomePage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { ThemeContext } from '@emotion/react';
+import React from 'react';
 
 
 function App() {
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme}}>
-      <div className='App' id='theme'>
+      <div className='App'>
         <Router>
           <Header />
             <Routes>
-              <Route path="/home" exact component={HomePage} />
+              <Route path="/" exact component={HomePage} />
             </Routes>
         </Router>
       </div>
-    </ThemeContext.Provider>
-  )
+  );
 }
+
 
 export default App;
--- Front-end/src/components/Header/Header.js
+++ Front-end/src/components/Header/Header.js
@@ -2,60 +2,57 @@ import React, { createContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import './Header.css';
 import { Link } from "react-router-dom";
-import { height, sizeWidth } from '@mui/system';
-import ReactSwitch from "react-switch";
+import { boxSizing, height, sizeWidth } from '@mui/system';
 
-export const ThemeContext = createContext(null);
+
 
 function Header () {
 
   const [openLinks, setOpenLinks] = useState(false);
 
-  const toggleNavBar = () => {
-    setOpenLinks(!openLinks);
-  };
-
-  const [theme, setTheme] = useState("dark");
-
-  const toggleTheme = () => {
-    setTheme((curr) => (curr === "light" ? "dark" : "light"));
-  };
+  // const toggleNavBar = () => {
+  //   setOpenLinks(!openLinks);
+  // };
 
 
   return (
     <div className='navBar'>
-
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       <div className='leftSide' id={openLinks ? "open" : "close"}>
         <div className='Home'>
-          <Link to="/home">Home</Link>
+          <Link to="/">Home</Link>
         </div>
       </div>
 
 
       <div className='middle'>
         <div className='Title'>
-          <h1>Muse360</h1>
+          <h1><u>Muse360</u></h1>
         </div>
       </div>
 
 
       <div className='rightSide'>
         <div className='SearchBar'>
-          <img
-            width={15}
-            height={15}
-            src="https://iconarchive.com/download/i60242/zerode/plump/Search.ico"
-            alt='search icon image'
-          />
           <input type="text" placeholder="Search..."></input>
+          <button type="submit"><i class="fa fa-search"></i></button>
         </div>
 
-        <div className='DarkModeButton'>
-          <label>
-            {theme === "light" ? "Light Mode" : "Dark Mode"}
-          </label>
-          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
-        </div>
+        {/* DARK MODE BUTTON */}
+
+        {/* <div className='DarkModeButton'>
+          <ThemeContext.Consumer>
+            {({changeTheme}) => (
+              <DarkModeToggle
+                color="link"
+                onChange={() => {
+                  changeTheme(darkMode ? themes.light : themes.dark);
+                }}
+              >
+                <i className={darkMode}
+
+            )}
+          </ThemeContext.Consumer> */}
       </div>
 
     </div>
