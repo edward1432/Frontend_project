a12de8ea33efac7eed39ff44891ce84675d61591
Edward Todd <edwardtdd@gmail.com>
2022-06-21 09:28:32 +0100

    Added museum class

---
+++ Front-end/src/components/Museums/Museum.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Museum.css';
+
+const Museum = (museum) => (
+  <div className="Museum">
+    Museum Component
+  </div>
+);
+
+Museum.propTypes = {};
+
+Museum.defaultProps = {};
+
+export default Museum;
