c55e121764672110e731310a45eb43592ee9b2c4
alh40 <alexander.leaver@me.com>
2022-06-21 11:16:36 +0100

    start point

--- Front-end/src/components/Museums/Museum.js
+++ Front-end/src/components/Museums/Museum.js
@@ -1,15 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Museum.css';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
 
-const Museum = (museum) => (
-  <div className="Museum">
-    Museum Component
-  </div>
-);
+const Museum = () => {
+  const [museums, setMuseums] = useState([]);
 
-Museum.propTypes = {};
+  useEffect((getAllMuseums) => {
+    axios.get('http://127.0.0.1:8080/museum')
+      .then(res => {
+        const museums = res.data;
+        setMuseums(museums);
+      }).catch((err) => console.log(err));
+    }, []
+  );
 
-Museum.defaultProps = {};
+  return (
+    <ul>
+      {
+        museums.map(museum =>
+          <li key={museum.id}>{`${museum.name} | ${museum.country}`}</li>
+          )
+      }
+    </ul>
+  )
+  
+
+}
+
+const Museum = () => {
+  const [museums, setMuseums] = useState([]);
+
+  useEffect((getAllMuseums) => {
+    axios.get('http://127.0.0.1:8080/museum')
+      .then(res => {
+        const museums = res.data;
+        setMuseums(museums);
+      }).catch((err) => console.log(err));
+    }, []
+  );
+
+  return (
+    <ul>
+      {
+        museums.map(museum =>
+          <li key={museum.id}>{`${museum.name} | ${museum.country}`}</li>
+          )
+      }
+    </ul>
+  )
+  
+
+}
+
 
 export default Museum;
