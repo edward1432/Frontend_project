7dbdb5dcec95a00bcdeca5521dd0da4c85d1cf9a
Edward Todd <edwardtdd@gmail.com>
2022-06-21 09:30:50 +0100

    deleted museums class

--- Front-end/src/components/Museums/Museums.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Museums from './Museums';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Museums />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
