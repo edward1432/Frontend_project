7ffdbbd9855e999725c60dd86f8578daee5c6642
alh40 <alexander.leaver@me.com>
2022-06-21 13:27:40 +0100

    t

--- Front-end/src/Services/MuseumService/MuseumDeleteById.js
+++ Front-end/src/Services/MuseumService/MuseumDeleteById.js
@@ -17,7 +17,7 @@ const MuseumDeleteById = () => {
     "id": id,
   }
 
-  axios.post(`http://127.0.0.1:8080/museum/${museum}`)
+  axios.delete(`http://127.0.0.1:8080/museum/${museum}`)
   .then(res =>{
     console.log(res);
   }).catch((err) => console.log(err));
