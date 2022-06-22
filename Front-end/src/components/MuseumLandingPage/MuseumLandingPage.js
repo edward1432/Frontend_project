8a208b70186aab131fac75ebcb05d5e1d2185627
alh40 <alexander.leaver@me.com>
2022-06-22 11:28:55 +0100

    m

--- Front-end/src/components/MuseumLandingPage/MuseumLandingPage.js
+++ Front-end/src/components/MuseumLandingPage/MuseumLandingPage.js
@@ -4,14 +4,12 @@ import MuseumCreate from '../Museums/Axios/MuseumCreate';
 import MuseumDeleteById from '../Museums/Axios/MuseumDeleteById';
 import MuseumGetById from '../Museums/Axios/MuseumGetById';
 import MuseumList from '../Museums/MuseumList';
-import TableData from '../Museums/MuseumTable/MuseumForm';
 function MuseumLandingPage () {
     
   return(
     <>
       <div className="MuseumLandingPage">
         <MuseumList />
-        <TableData />
         <MuseumGetById />
         <MuseumCreate />
       </div>
