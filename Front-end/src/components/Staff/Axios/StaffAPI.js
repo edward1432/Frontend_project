3e26b1cd2acd8f43168d2f5076dc3d893a087389
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2022-06-22 15:41:12 +0100

    get all staff method

---
+++ Front-end/src/components/Staff/Axios/StaffAPI.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+export function getAllStaff (setStaff) {
+    axios.get('http://127.0.0.1:8080/staff')
+    .then(res => {
+      const staff = res.data;
+      setStaff(staff);
+    }).catch((err) => console.log(err));
+}
+
+export function getExhibitById (setExhibit, id) {
+    axios.get(`http://127.0.0.1:8080/exhibit/${id}`)
+  .then(res =>{
+    console.log(res);
+    getAllExhibits(setExhibit);
+  }).catch((err) => console.log(err));
+}
+
+export function createExhibit (setExhibits, exhibit) {
+    axios.post(`http://127.0.0.1:8080/exhibit/create?name=${exhibit.name}&museum_id=${museum_id}`)
+    .then(res =>{
+      console.log(res);
+      getAllExhibits(setExhibits);
+    }).catch((err) => console.log(err));
+}
+
+export function updateExhibit (setExhibit, exhibit) {
+    axios.put(`http://127.0.0.1:8080/exhibit/update/${id}?&name=${exhibit.name}`)
+  .then(res =>{
+    console.log(res);
+    getAllExhibits(setExhibit);
+  }).catch((err) => console.log(err));
+}
+
+export function deleteExhibit (setExhibit, id) {
+    axios.delete(`http://127.0.0.1:8080/exhibit/delete/${id}`)
+  .then(res =>{
+    console.log(res);
+    alert(`Exhibit deleted`);
+    getAllExhibits(setExhibits);
+  }).catch((err) => console.log(err));
+}
