b9d3f2b0d013ebd101b139acede04200ce947b9d
Alexander Leaver Hernandez <54749323+alh40@users.noreply.github.com>
2022-06-22 13:45:31 +0100

    Revert "a"

--- Front-end/src/components/Exhibits/Axios/ExhibitAPI.js
+++ Front-end/src/components/Exhibits/Axios/ExhibitAPI.js
@@ -1,20 +1,5 @@
 import axios from 'axios';
 
-
-export function getExhibits(setExhibits) {
-  axios.get('http://127.0.0.1:8080/exhibit').then(res => {
-    const exhibits = res.data;
-    setExhibits(exhibits);
-  }).catch((err) => console.log(err));
-}
-export function deleteExhibit(setExhibits, id) {
-    axios.delete(`http://127.0.0.1:8080/exhibit/delete/${id}`)
-    .then(res => {
-      alert(`Exhibit deleted`);
-      getExhibits(setExhibits);
-    }).catch((err) => console.log(err));
-  }
-
 export function getAllExhibits (setExhibits) {
     axios.get('http://127.0.0.1:8080/exhibit')
     .then(res => {
@@ -55,4 +40,3 @@ export function deleteExhibit (setExhibit, id) {
     getAllExhibits(setExhibit);
   }).catch((err) => console.log(err));
 }
-
