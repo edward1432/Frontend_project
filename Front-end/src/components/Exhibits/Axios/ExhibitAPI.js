e8298bbc9f5361ffa0830dbae9da3a4158cb1d54
Edward Todd <edwardtdd@gmail.com>
2022-06-22 14:11:22 +0100

    a

--- Front-end/src/components/Exhibits/Axios/ExhibitAPI.js
+++ Front-end/src/components/Exhibits/Axios/ExhibitAPI.js
@@ -31,28 +31,19 @@ export function getExhibitById (setExhibit, id) {
   }).catch((err) => console.log(err));
 }
 
-export function createExhibit (setExhibits, exhibit) {
-    axios.post(`http://127.0.0.1:8080/exhibit/create?name=${exhibit.name}&museum_id=${museum_id}`)
-    .then(res =>{
-      console.log(res);
-      getAllExhibits(setExhibits);
-    }).catch((err) => console.log(err));
-}
+// export function createExhibit (setExhibits, exhibit) {
+//     axios.post(`http://127.0.0.1:8080/exhibit/create?name=${exhibit.name}&museum_id=${museum_id}`)
+//     .then(res =>{
+//       console.log(res);
+//       getAllExhibits(setExhibits);
+//     }).catch((err) => console.log(err));
+// }
 
-export function updateExhibit (setExhibit, exhibit) {
-    axios.put(`http://127.0.0.1:8080/exhibit/update/${id}?&name=${exhibit.name}`)
-  .then(res =>{
-    console.log(res);
-    getAllExhibits(setExhibit);
-  }).catch((err) => console.log(err));
-}
-
-export function deleteExhibit (setExhibit, id) {
-    axios.delete(`http://127.0.0.1:8080/exhibit/delete/${id}`)
-  .then(res =>{
-    console.log(res);
-    alert(`Exhibit deleted`);
-    getAllExhibits(setExhibit);
-  }).catch((err) => console.log(err));
-}
+// export function updateExhibit (setExhibit, exhibit) {
+//     axios.put(`http://127.0.0.1:8080/exhibit/update/${id}?&name=${exhibit.name}`)
+//   .then(res =>{
+//     console.log(res);
+//     getAllExhibits(setExhibit);
+//   }).catch((err) => console.log(err));
+// }
 
