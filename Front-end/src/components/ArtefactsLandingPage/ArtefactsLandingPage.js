1beb2ff6efc4195e8cda8177066fff0d9f6b2dde
Edward Todd <edwardtdd@gmail.com>
2022-06-23 16:18:17 +0100

    gif

--- Front-end/src/components/ArtefactsLandingPage/ArtefactsLandingPage.js
+++ Front-end/src/components/ArtefactsLandingPage/ArtefactsLandingPage.js
@@ -10,10 +10,9 @@ function ArtefactsLandingPage () {
         <h1 class = "name">Artefacts</h1>
         <ArtefactsTable />
       </div>
-
       <div class = 'artefact-gif'>
-    <img src="https://media2.giphy.com/media/uULru6cnBO4gM/giphy.gif?cid=ecf05e4730vk3i70ewcbawj9m5z3x2h73lcq8lgbmaiff1kn&rid=giphy.gif&ct=g" width = '200px'/>
-</div>
+        <img src="https://media2.giphy.com/media/uULru6cnBO4gM/giphy.gif?cid=ecf05e4730vk3i70ewcbawj9m5z3x2h73lcq8lgbmaiff1kn&rid=giphy.gif&ct=g" width = '200px'/>
+      </div>
     </>
   )
   };
--- Front-end/src/components/Header/Header.js
+++ Front-end/src/components/Header/Header.js
@@ -4,10 +4,7 @@ import './Header.css';
 import { Link } from "react-router-dom";
 import { boxSizing, height, sizeWidth } from '@mui/system';
 
-
-
 function Header () {
-
   const [openLinks, setOpenLinks] = useState(false);
 
   // const toggleNavBar = () => {
@@ -22,7 +19,6 @@ function Header () {
     setIsClicked(isClicked => !isClicked);
   }
 
-
   return (
     <div className='topnav' id = "myTopnav">
       <div class="123" id="123">
@@ -30,8 +26,6 @@ function Header () {
       <div className='leftSide' id={openLinks ? "open" : "close"}>
         <div class = "leftBar">
           <div class = "Row">
-           
-
         <div class="dropdown">
     <button class="drop-button">
       <i class="fa fa-navicon"></i>
@@ -58,13 +52,11 @@ function Header () {
       </div>
       </div>
 
-
       <div className='middle'>
         <div className='Title'>
-          <h1><u>Museum Management Tool</u></h1>
+          <h1><u>Museum Management System</u></h1>
         </div>
       </div>
-
 
       <div className='rightSide'>
         <div className='SearchBar'>
@@ -72,12 +64,7 @@ function Header () {
           <button type="submit"><i class="fa fa-search"></i></button>
           <button className="dark-mode-button" onClick={darkModebutton}>{isClicked ? "Light" : "Dark"}</button>
         </div>
-
-        
-
-      
       </div>
-
     </div>
   )
 }
