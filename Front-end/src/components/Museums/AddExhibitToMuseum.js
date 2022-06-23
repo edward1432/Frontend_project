fc6d70e5c1613e03ac915d2cfd8e9195110eed50
Edward Todd <edwardtdd@gmail.com>
2022-06-24 00:48:20 +0100

    Improved things

--- Front-end/src/components/Museums/AddExhibitToMuseum.js
+++ Front-end/src/components/Museums/AddExhibitToMuseum.js
@@ -2,39 +2,38 @@ import { useState, useEffect } from 'react';
 import { addExhibit } from './Axios/MuseumAPI';
 
 
-const AddExhibitToMuseum = () => {
-    
-    const[museumID, setMuseumID] = useState(0)
-    const[exhibitID, setExhibitID] = useState(0)
-    const[museums, setMuseums] = useState([])
-    
-    const handleMuseumIdChange = event => setMuseumID(event.target.value);
+const AddExhibitToMuseum = ({ musID }) => {
+
+    const [exhibitID, setExhibitID] = useState(0)
+    const [museums, setMuseums] = useState([])
+
+    // const handleMuseumIdChange = event => setMuseumID(event.target.value);
     const handleExhibitIdChange = event => setExhibitID(event.target.value);
 
     const handleSubmit = event => {
-      
-        addExhibit(museumID, exhibitID, setMuseums)
+
+        addExhibit(musID, exhibitID, setMuseums)
     }
-    
+
     return (
-    
-        <form onSubmit = {handleSubmit}>
-            <div class = "update-museum-form">
-                <label>
+
+        <form onSubmit={handleSubmit}>
+            <div class="update-museum-form">
+                {/* <label>
                     Museum ID:
                     <input type = "text" name = "museum-id" onChange = {handleMuseumIdChange}></input>
-                </label>
+                </label> */}
                 <label>
                     Exhibit ID:
-                    <input type = "text" name = "exhibit-id" onChange = {handleExhibitIdChange}></input>
+                    <input type="text" name="exhibit-id" onChange={handleExhibitIdChange}></input>
                 </label>
-                <button class = "update-museum" type = "submit" id = "button">Update Museum</button>
+                <button class="update-museum" type="submit" id="button">Add exhibit to museum</button>
             </div>
 
         </form>
-    
-    
-        )
+
+
+    )
 }
 
 export default AddExhibitToMuseum;
--- Front-end/src/components/Museums/MuseumPage.js
+++ Front-end/src/components/Museums/MuseumPage.js
@@ -1,39 +1,41 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { getMuseum } from "./Axios/MuseumAPI";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import AddExhibitToMuseum from "./AddExhibitToMuseum";
 
-function MuseumPage () {
+function MuseumPage() {
 
     const params = useParams();
     const [museum, setMuseum] = useState({});
     const [exhibits, setExhibits] = useState([]);
 
-    if (!museum.id){
-    getMuseum(params.id, setMuseum);
+    if (!museum.id) {
+        getMuseum(params.id, setMuseum);
     }
 
     useEffect(() => {
         setExhibits(museum.exhibits);
     })
 
-    return(
+    return (
         <>
-        <section>
-        <div className = "museum-page">
-            <h1>{museum.name}</h1>
-        </div>
-        <div>Exhibits:
-            <ul>
-                {exhibits && exhibits.map(exhibit => 
-                    <li key={exhibit.id}>
-                        <a href={"/exhibitpage/" + exhibit.id}>{exhibit.name}</a>
-                    </li>)}
-            </ul>
-        </div>
-        <AddExhibitToMuseum />
-        </section>
+            <section>
+                <div className="museum-page">
+                    <h1>{museum.name}</h1>
+                </div>
+                <div>Exhibits:
+                    <ul>
+                        {exhibits && exhibits.map(exhibit =>
+                            <li key={exhibit.id}>
+                                <a href={"/exhibitpage/" + exhibit.id}>{exhibit.name}</a>
+                            </li>)}
+                    </ul>
+                </div>
+                <div>
+                    <AddExhibitToMuseum musID={museum.id} />
+                </div>
+            </section>
         </>
     )
 }
